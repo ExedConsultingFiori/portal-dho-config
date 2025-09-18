1d386ad3de87fdc6c399dc7743eb34daabd741d0
RanierDalton <ranier.couto@sptech.school>
2025-09-18 14:42:28 -0300

    feat: Adicionando lógica de requisição informações processos

--- portal-dho/webapp/controller/Processos.controller.js
+++ portal-dho/webapp/controller/Processos.controller.js
@@ -10,19 +10,16 @@ sap.ui.define([
 	'sap/ui/model/odata/v2/ODataModel'
 ], function (JSONModel, Controller, Button, Dialog, MessageToast, Text) {
 	"use strict";
-	return Controller.extend("portaldho.controller.portal.controller", {
+	return Controller.extend("portaldho.controller.Processos.controller", {
 
-        // Funções Relacionadas ao Conteúdo do Usuário Logado
-		onInit: function () {
-		},
-        onClickMeusDados: () =>{
+		onInit: () => {
             let numeroPessoalUsuarioLogado = "00000001";
             let oView = this.getView();
             let oModelUsuarioLogado = new JSONModel(); // Instância vazia do objeto que irá receber os dados do usuário
             let oModelRequisicao = this.getOwnerComponent().getModel();
 
             // Método read
-            oModelRequisicao.read(`/ZI_DADOS_PESSOAISSet(NumPessoal='${numeroPessoalUsuarioLogado}')`, {
+            oModelRequisicao.read(`/ZI_PROCESSOSSet(NumPessoal='${numeroPessoalUsuarioLogado}')`, {
                 success: (oData, response) => {
                     oModelUsuarioLogado.setData(response.data);
                     oView.setModel(oModelUsuarioLogado, "usuarioLogado");
@@ -31,21 +28,6 @@ sap.ui.define([
                     console.error("Erro ao obter os dados:", oError);
                 }
             });
-        },
-		handleEditToggled: function (oEvent) {
-			// just dummy function to activate input validation in SmartForm.
 		},
-
-        // Funções Relacionadas ao Side Menu
-        onCollapseExpandPress() {
-			const oSideNavigation = this.byId("sideNavigation"),
-			bExpanded = oSideNavigation.getExpanded();
-
-			oSideNavigation.setExpanded(!bExpanded);
-		},
-		handleEditToggled: function (oEvent) {
-			// just dummy function to activate input validation in SmartForm.
-		},
-        
 	});
 });
